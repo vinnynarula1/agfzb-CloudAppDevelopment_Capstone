2ea42c0e55843b70ada9b6307e8d09836f0de011
vinnynarula1 <vinny.narula7@gmail.com>
2021-10-19 00:30:06 +0000

    some changes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -5,18 +5,31 @@ from django.utils.timezone import now
 # Create your models here.
 
 # <HINT> Create a Car Make model
-class CarMake(models.Model)
-    name = models.CharField(null=False, max_length=30, default='Car Make')
-    description = models.CharField(max_length=1000)
-   
 
+class CarMake(models.Model):
+    name = models.CharField(null=False,max_length=1000)
+    description = models.CharField(max_length=3000)
     def __str__(self):
-        return "Car Model: " + self.name + "," + \
+        return "Name: " + self.name + ", " + \
                "Description: " + self.description
 
 
-
 class CarModel(models.Model):
+
+    carmake = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False,max_length=1000)
+    dealer_id = models.IntegerField()
+    SEDAN = 'sedan'
+    SUV = 'SUV'
+    WAGON = 'wagon'
+    HATCHBACK = 'hatchback'
+    type_choices = [(SEDAN, 'Sedan'),(SUV, 'SUV'),(WAGON, 'Wagon'),(HATCHBACK, 'Hatchback')]
+    car_type = models.CharField(null=False,choices=type_choices,max_length=1000)
+    year = models.DateField()
+    def __str__(self):
+        return "Name: " + self.name + ", " + "Type: " + self.car_type + ', ' + 'Year: ' + str(self.year)
+    
+
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
 # - Dealer id, used to refer a dealer created in cloudant database
@@ -53,3 +66,24 @@ class CarDealer:
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+    def __init__(self, car_make, car_model, car_year, dealership, name, purchase, purchase_date, review, sentiment):
+        self.car_make= car_make
+        #Dealer car model
+        self.car_model = car_model
+        #Dealer car year
+        self.car_year = car_year
+        #Dealer name
+        self.dealership = dealership
+        #Dealer name
+        self.name = name
+        # Location purchase
+        self.purchase = purchase
+        # Dealer purchase_date
+        self.purchase_date = purchase_date
+        # Dealer review
+        self.review = review
+        # Dealer zip
+        self.sentiment  = sentiment
+    def __str__(self):
+        return "Dealer Name: " +self.full_name
