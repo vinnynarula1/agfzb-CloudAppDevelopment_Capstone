8e8bbddcb845844c6df946e8fdfa930e5aaac710
vinnynarula1 <vinny.narula7@gmail.com>
2022-01-02 07:57:02 +0000

    more

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
 
     path(route='', view=views.get_dealerships, name='index'),
 
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+
     # path for dealer reviews view
 
     # path for add a review view
