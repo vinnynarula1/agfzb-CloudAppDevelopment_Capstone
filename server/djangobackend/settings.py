4d6a6ab37131143c304a1b8dcd9d58450e2e24da
vinnynarula1 <84547910+vinnynarula1@users.noreply.github.com>
2022-02-09 15:34:41 -0800

    changes made

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'djangobackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME' : 'postgres',
-        'USER' : 'postgres',
-        'PASSWORD': 'NjM4NC12aW5ueW5h',
-        'HOST' : 'localhost',
-        'PORT' : '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
